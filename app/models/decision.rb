e99a297e5c99d2fb44de2b8e3f13fa3a997720ff
James Macapagal <j.macapagal@gmail.com>
2022-08-09 16:27:34 -0500

    added belong_to Decision for self join

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -1,5 +1,7 @@
 class Decision < ApplicationRecord
     has_one :sub_decision, foreign_key: :prev_devision_id, class_name: 'Decision'
+    belongs_to :prev_decision, class_name: "Decision", optional: true
+
     has_one :outcomes
 
 end
