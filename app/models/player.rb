3be4d223281b72eb96c63cd0862e5ae29b9319c8
James Macapagal <j.macapagal@gmail.com>
2022-08-22 10:14:51 -0500

    adjusting username and email validations

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,5 @@
 class Player < ApplicationRecord
+    require 'obscenity/active_model'
     has_many :scores, dependent: :destroy
     has_many :games, through: :scores
 
@@ -7,4 +8,5 @@ class Player < ApplicationRecord
     validates :username,:password,:email, presence: true
     validates :username,:email, uniqueness: true
     validates :username, :password, :email, format: { without: /\s/}
+    validates :username, :email, obscenity: {message: "... Naughty, naughty ..."}
 end
